c7873684da946d9de2c4766968e48c993c04fcf9
Tyson Parks <typarks@gmail.com>
2023-03-09 10:16:13 -0800

    add fileSizeTester.js

---
+++ testing/fileSizeTester.js
@@ -0,0 +1,43 @@
+
+function gridRowIndices(x, y) {
+  return array.map((x) => x.a + x.b)
+}
+
+
+function gridRowIndices(x, y) {
+  return array.map((x) => x.a + x.b)
+}
+
+
+function gridRowIndices(x, y) {
+  return array.map((x) => x.a + x.b)
+}
+
+// constructor(start, end) {
+  //   if (arguments.length === 1) {
+  //     if (start instanceof Array) {
+  //       if (start[0] instanceof Array) {
+  //         this.verts = { start: vert(start[0]), end: vert(start[1]) }
+  //       }
+  //       if (start[0] instanceof Object) {
+  //         this.verts = { start: start.start, end: start.end }
+  //       }
+  //       if (start[0] instanceof Vector) {
+  //         this.verts = { start: start[0], end: start[1] }
+  //       }
+  //     }
+  //     if (start instanceof Object) {
+  //       this.verts = { start: start.start, end: start.end }
+  //     }
+  //   } else {
+  //     this.verts = { start: start, end: end }
+  //   }
+
+
+  //   // if (!(verts[0] instanceof Vertex)) {
+  //   //   verts = [vert(verts[0]), vert(verts[1])]
+  //   // }
+  //   // this.verts = { start: start, end: end }
+  //   // this.lineVector = this.#getVector(verts)
+  //   // this.lineVector = p5.Vector.sub(this.endPoint, this.startPoint)
+  // }
