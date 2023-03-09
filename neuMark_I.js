2376baa6900759abf89039ccfb54142b4ca5c9e6
Tyson Parks <typarks@gmail.com>
2023-03-09 10:12:44 -0800

    Add neuMark_I.js

---
+++ neuMark_I.js
@@ -0,0 +1,519 @@
+// MARK: CSS Factory Functions
+
+// TODO: after most work is complete check to see how many of these are actually used and clean where needed
+// CLASS: CS
+class CS {
+  static inset = 'inset'
+  // static space = ' '
+  static comma = ', '
+
+  static boxShadow = 'box-shadow'
+  // static dropShadow = 'drop-shadow'
+  static textShadow = 'text-shadow'
+
+  static transform = 'transform'
+  static border = 'border'
+  static borderRadius = 'border-radius'
+  static rotate = 'rotate'
+  static scale = 'scale'
+  static filter = 'filter'
+  static padding = 'padding'
+  static clipPath = 'clip-path'
+  static shapeOutside = 'shape-outside'
+  static fontSize = 'font-size'
+  static textAlign = 'text-align'
+  static vertAlign = 'vertical-align'
+  static lineHeight = 'line-height'
+  static margin = 'margin'
+  // static width = 'width'
+  static background = 'background'
+  static backgroundColor = 'background-color'
+  static color = 'color'
+  static clear = "#0000"
+  static display = 'display'
+  static overflow = 'overflow'
+  static flexDirection = 'flex-direction'
+  static justifyContent = 'justify-content'
+  static alignItems = 'align-items'
+}
+
+// MARK:CSS Look Class
+
+// PROTOTYPE: p5.Element.prototype.look()
+p5.Element.prototype.look = function (look = Look.testGrid, html) {
+  if (look instanceof Array) {
+    const lookOp = OpArray.from(look).compacted
+    lookOp.forEach(e => this.style(e[0], e[1]))
+  }
+  if (html) { this.html(html) }
+  return this
+}
+
+// CLASS: Look
+class Look {
+
+  static islandShape(path, color = randomColor().toString('#rrggbbaa')) {
+    return [
+      [CS.backgroundColor, testingControls.blackMode ? '#000' : color],
+      [CS.clipPath, path],
+      [CS.shapeOutside, path],
+      // [CS.overflow, 'visible']
+    ]
+  }
+
+  static neuShade(
+    {
+      baseCol = protoColor(230),
+      vector = globalShadowVector(),
+      start = globalControls.start,
+      spread = globalControls.spread,
+      inset = globalControls.inset,
+    } = {}
+  ) {
+    return [[CS.boxShadow, Shade.neuBoxShadFactory(baseCol, vector, start, spread, inset)]]
+  }
+
+  static textAlign({ size, hor = 'center', vert = 'center' } = {}) {
+    let textAlign = [CS.textAlign, hor]
+    let lineHeight = [CS.lineHeight, `${size.y}px`]
+    let otherHeight = [CS.lineHeight, `${(size.y + (sqrt(size.x) * 2.5))}px`]
+    if (vert === 'center') { return [textAlign, lineHeight] }
+    if (vert === 'top') { return [textAlign] }
+    if (vert === 'below') { return [textAlign, otherHeight] }
+  }
+
+  static testText(
+    sizeX,
+    color,
+    radius = 25,
+    labels = testingControls.labels,
+    borders = testingControls.borders,
+  ) {
+    return [
+      [CS.color, (labels ? color : CS.clear)],
+      // [CS.borderRadius, `${sqrt(sizeX) * 1.5}px`],
+      [CS.borderRadius, `${radius}px`],
+      [CS.fontSize, `${sqrt(sizeX) * 1.5}px`],
+      [CS.border, `${borders ? color : CS.clear} dashed ${sqrt(sizeX) / 20}px`],
+    ]
+  }
+
+  static test(size, type) {
+    switch (type) {
+      case 'frame':
+        return [...Look.textAlign({ size: size, vert: 'top' }), ...Look.testText(size.x, '#80F8', 50)]
+      case 'grid':
+        return [...Look.textAlign({ size: size, vert: 'top', hor: 'start' }), ...Look.testText(size.x, '#08F8')]
+      case 'group':
+        // return []
+        return [...Look.textAlign({ size: size, hor: 'start', vert: 'top', }), ...Look.testText(size.x, '#80F0')]
+      case 'cell':
+        const text = [
+          ...Look.textAlign({ size: size }),
+          ...Look.testText(size.x, '#8088')
+        ]
+        const neuShade = [...Look.neuShade()]
+        if (testingControls.blackMode) { return text }
+        else { return [...text, ...neuShade] }
+        return [
+          ...Look.textAlign({ size: size }),
+          ...Look.testText(size.x, '#8088'),
+          ...Look.neuShade()
+        ]
+      case 'island':
+        return [...Look.textAlign({ size: size }), ...Look.testText(size.x, '#F08b')]
+      case 'shape':
+        return [...Look.textAlign({ size: size, vert: 'below' }), ...Look.testText(size.x, '#F0Fb')]
+    }
+  }
+
+  static get testGrid() {
+    return [
+      // [CS.color, '#80F'],
+      [CS.color, CS.clear],
+      [CS.backgroundColor, '#00'],
+      [CS.borderRadius, '10%'],
+      [CS.textAlign, "center"],
+      // [CS.border, '#F7F dashed 1px'],
+      // [CS.display, 'none'],
+    ]
+  }
+
+  static testCell(color = randomColor().toString('#rrggbbaa'), labels = testingControls.labels,) {
+    // let randomColor = randomColor().toString('#rrggbbaa')
+    return [
+      [CS.color, (labels ? '#00F' : CS.clear)],
+      [CS.backgroundColor, '#0FF6'],
+      [CS.backgroundColor, color],
+      [CS.borderRadius, '20%'],
+      [CS.textAlign, "center"],
+      // [CS.border, '#808 solid 1px'],
+    ]
+  }
+
+  static blankTestCell(labels = testingControls.labels,) {
+    const neuShade = [...Look.neuShade({ vector: globalShadowVector().mult(0.3), inset: !globalControls.inset, })]
+    return [
+      [CS.color, (labels ? '#00F' : CS.clear)],
+      [CS.backgroundColor, CS.clear],
+      [CS.borderRadius, '50%'],
+      [CS.textAlign, "center"],
+      testingControls.blackMode ? neuShade : null,
+      // neuShade,
+      // ...Look.neuShade({ vector: globalShadowVector().mult(0.3), inset: !globalControls.inset, })
+    ]
+  }
+
+  static centeredFlex(color, direction = 'row') {
+    return [
+      [CS.background, color],
+      [CS.margin, '0'],
+      [CS.display, 'flex'],
+      [CS.flexDirection, direction],
+      [CS.justifyContent, 'space-evenly'],
+      [CS.alignItems, 'center'],
+    ]
+  }
+
+}
+
+// TODO: can these functions be generalized into Classes? Or proto extensions on p5.Element?
+// MARK:CSS Shadow Functions
+
+// PROTOTYPE: p5.Element.prototype.boxShadow
+p5.Element.prototype.boxShadow = function (value) {
+  return this.style(boxShadow, value)
+}
+
+// CLASS: Shade
+class Shade {
+
+  //shadowVector: create vector from Angle + Offset
+  static shadVect(angle = 45, offset = 8) { return createVector(1, 0).rotate(angle).mult(offset) }
+  static maxComponent(vector = this.shadVect()) {
+    return vector.y
+    // return max(this.x, this.y)
+  }
+
+  // Box-Shadow CSS
+  static boxShadCSS(x, y, blurRad = 0, spreadRad = 0, col = color(0), inset = false) {
+    let color = col.toString('#rrggbb')
+    if (inset === true) {
+      return `inset ${x}px ${y}px ${blurRad}px ${color}`
+    } else if (spreadRad === 0) {
+      return `${x}px ${y}px ${blurRad}px ${color}`
+    } else {
+      return `${x}px ${y}px ${blurRad}px ${spreadRad}px ${color}`
+    }
+  }
+
+  // Text-Shadow CSS
+  static textShadCSS(x, y, blurRad = 0, col = color(0)) { return this.boxShadCSS(x, y, blurRad, col) }
+
+  // Drop-Shadow CSS
+  static dropShadCSS(x, y, blurRad = 0, col = color(0)) {
+    return `drop-shadow(${this.boxShadCSS(x, y, blurRad, col)})`
+  }
+
+  //Neumorphic Box-Shadow CSS - create highlight shadow pair code for CSS
+  static neuBoxShadCSS(vector = this.shadVect(), blurRad, highCol, shadCol, inset = false) {
+    let highlightCSS = this.boxShadCSS(-vector.x, -vector.y, blurRad, 0, highCol, inset)
+    let shadowCSS = this.boxShadCSS(vector.x, vector.y, blurRad, 0, shadCol, inset)
+    return `${shadowCSS}, ${highlightCSS}`
+  }
+
+  // Neumorphic Box Shadow Factory - create a shadow and highlight stack
+  static neuBoxShadFactory(baseCol = protoColor(230), vector = this.shadVect(), start = 0.5, spread = 16, inset = false) {
+    let offset = vector.mag() / sqrt(2)
+    // print(`offset: ${offset}`)
+    let cols = baseCol.highShadSpread(spread)
+    let neuShads = cleanSlices(start, offset, globalControls.shadQuality)
+    // print(neuShads.map(e => e.toFixed(2))) 
+    neuShads = neuShads.map(sliceOffset => this.neuBoxShadCSS(vector.setMag(sliceOffset), 2 * sliceOffset, cols[0], cols[1], inset))
+    return neuShads
+  }
+
+
+}
+
+//TODO: DEPRECATE
+// MARK: OLD CSS Shadow Functions
+//#region OLD CSS Shadow Functions
+// FUNC: shadowVector()
+//shadowVector: create vector from Angle + Offset
+// function shadowVector(angle = 45, offset = 8) {
+//   return createVector(1, 0).rotate(angle).mult(offset)
+// }
+
+//TODO: DEPRECATE
+// FUNC: boxShadowCSS()
+// Box-Shadow CSS
+function boxShadowCSS(x, y, blurRad = 0, spreadRad = 0, col = color(0), inset = false) {
+  let color = col.toString('#rrggbb')
+  if (inset === true) {
+    return `inset ${x}px ${y}px ${blurRad}px ${color}`
+  } else if (spreadRad === 0) {
+    return `${x}px ${y}px ${blurRad}px ${color}`
+  } else {
+    return `${x}px ${y}px ${blurRad}px ${spreadRad}px ${color}`
+  }
+}
+
+//TODO: DEPRECATE
+// FUNC: textShadowCSS()
+// Text-Shadow CSS
+function textShadowCSS(x, y, blurRad = 0, col = color(0)) {
+  return boxShadowCSS(x, y, blurRad, col)
+}
+
+//TODO: DEPRECATE
+// FUNC: dropShadowCSS()
+// Drop-Shadow CSS
+function dropShadowCSS(x, y, blurRad = 0, col = color(0)) {
+  return `drop-shadow(${boxShadowCSS(x, y, blurRad, col)})`
+}
+
+//TODO: DEPRECATE
+// FUNC: maxComponent()
+function maxComponent(vector = Shade.shadVect()) {
+  return vector.y
+  // return max(this.x, this.y)
+}
+
+//TODO: DEPRECATE
+// FUNC: neuBoxShadowCSS()
+//Neumorphic Box-Shadow CSS - create highlight shadow pair code for CSS
+function neuBoxShadowCSS(vector = Shade.shadVect(), blurRad, highCol, shadCol, inset = false) {
+  let highlightCSS = boxShadowCSS(-vector.x, -vector.y, blurRad, 0, highCol, inset)
+  let shadowCSS = boxShadowCSS(vector.x, vector.y, blurRad, 0, shadCol, inset)
+  return `${shadowCSS}, ${highlightCSS}`
+  // return join([shadowCSS, highlightCSS], CS.comma)
+}
+
+//TODO: DEPRECATE
+// FUNC: neuBoxShadowFactory()
+// Neumorphic Box Shadow Factory - create a shadow and highlight stack
+function neuBoxShadowFactory(baseCol = GRAY, vector = Shade.shadVect(), start = 0.5, spread = 16, inset = false) {
+  let offset = vector.mag() / sqrt(2)
+  // print(`offset: ${offset}`)
+  let cols = hiShSpreadfromCol(spread, baseCol)
+  let neuShads = createSlices(start, offset, globalControls.shadQuality)
+  // print(neuShads.map(e => e.toFixed(2))) 
+  neuShads = neuShads.map(sliceOffset => neuBoxShadowCSS(vector.setMag(sliceOffset), 2 * sliceOffset, cols[0], cols[1], inset))
+  return neuShads
+}
+
+//TODO: DEPRECATE
+// FUNC: neuDropShadowFactory()
+function neuDropShadowFactory(baseCol = GRAY, vector = Shade.shadVect(), start = 0.5, spread = 16) {
+  let offset = vector.mag() / sqrt(2)
+  // print(`offset: ${offset}`)
+  let cols = hiShSpreadfromCol(spread, baseCol)
+  let neuShads = createSlices(start, offset, globalControls.shadQuality)
+
+}
+
+//TODO: DEPRECATE
+//MARK: OLD "offset" VARIANTS
+// #region OLD "offset" VARIANTS
+// FUNC: neuDropShadowFactory()
+//Neumorphic Box-Shadow CSS - create highlight shadow pair code for CSS
+function neuBxShCSS(offset, blurRad, hiCol, shCol, inset = false) {
+  let hiCSS = boxShadowCSS(-offset, -offset, blurRad, 0, hiCol, inset)
+  let shCSS = boxShadowCSS(offset, offset, blurRad, 0, shCol, inset)
+  return join([shCSS, hiCSS], CS.comma)
+}
+
+// FUNC: neuBxShFX()
+// Neumorphic Box Shadow Factory - create a shadow and highlight stack
+function neuBxShFX(baseCol, offset, start = 0.5, spread = 10, inset = false) {
+  let cols = hiShSpreadfromCol(spread, baseCol);
+  let neuShads = sliceExpSeries(start, offset)
+    .map(offset => neuBxShCSS(offset, offset * 2, cols[0], cols[1]))
+  // print(neuShads)
+  return neuShads
+}
+//#endregion
+//#endregion
+
+// TODO: can these functions be generalized into Classes? Or extensions on a Color class?
+// MARK: ProtoColor 
+
+function protoColor() {
+  if (arguments[0] instanceof p5.Color || arguments[0] instanceof ProtoColor) {
+    return arguments[0]; // Do nothing if argument is already a color object.
+  }
+
+  const args = arguments[0] instanceof Array ? arguments[0] : arguments
+  return new ProtoColor(this, args)
+}
+
+// CLASS: ProtoColor
+class ProtoColor extends p5.Color {
+  constructor(pInt, args) {
+    super(pInt, args)
+  }
+
+  get color() { return color(this.red, this.green, this.blue, this.alpha) }
+
+  get red() { return this._getRed() }
+  get green() { return this._getGreen() }
+  get blue() { return this._getBlue() }
+  get alpha() { return this._getAlpha() }
+
+  get hue() { return this._getHue() }
+  get saturation() { return this._getSaturation() }
+  get brightness() { return this._getBrightness() }
+  get lightness() { return this._getLightness() }
+
+  get complement() {
+    return protoColor(`hsba(${this.complementHue}, ${this.saturation}%, ${this.brightness}%, ${this.alpha})`)
+  }
+
+  get complementHue() { return (this.hue + 180 % 360) }
+
+  highShadComplementSpread(spread = 16) {
+    const h = this.hue
+    const s = this.saturation
+    const b = this.brightness
+
+    const high = [h, s, constrain(b + spread, 0, 100)]
+    const shad = [this.complementHue, s, constrain(b - 2 * spread, 0, 100)]
+
+    let cols = [high, shad]
+      .map(hsb => `hsb(${hsb[0]}, ${hsb[1]}%, ${hsb[2]}%)`)
+      .map(dscrpt => color(dscrpt))
+    return cols
+  }
+
+  highShadSpread(spread = 16) {
+    let b = this.brightness
+    let bPair = [round(b + spread), round(b - 2 * spread)]
+    let cols = bPair
+      .map(b => `hsb(${this.hue}, ${this.saturation}%, ${b}%)`)
+      .map(dscrpt => color(dscrpt))
+    return cols
+  }
+
+  static randomHighHue() {
+    let hue = floor(R.random_num(0, 255))
+    return protoColor(`hsb(${hue}, 100%, 100%)`)
+  }
+
+  static randomShadHue() {
+    let hue = floor(R.random_num(0, 255))
+    return protoColor(`hsb(${hue}, 100%, 50%)`)
+  }
+
+}
+
+//#region OLD Color functions
+//TODO: DEPRECATE
+// FUNC: hiShSpreadfromCol()
+// Highlight and Shadow colors spread from base color
+function hiShSpreadfromCol(spread = 16, baseCol = color(127)) {
+  let b = brightness(baseCol)
+  let bPair = [round(b + spread), round(b - 2 * spread)]
+  let cols = bPair
+    .map(b => `hsb(${hue(baseCol)}, ${saturation(baseCol)}%, ${b}%)`)
+    .map(dscrpt => color(dscrpt))
+  return cols
+}
+
+//TODO: DEPRECATE
+// FUNC: complimentHighShadSpread()
+function complimentHighShadSpread(spread = 16, baseCol = color(127)) {
+  let hueVal = round(hue(baseCol))
+  let satVal = saturation(baseCol)
+  let briVal = brightness(baseCol)
+
+  let highCol = [hueVal, satVal, constrain(briVal + spread, 0, 100)]
+  let shadCol = [complimentVal(hueVal), satVal, constrain(briVal - 2 * spread, 0, 100)]
+
+  let cols = [highCol, shadCol]
+    .map(hsb => `hsb(${hsb[0]}, ${hsb[1]}%, ${hsb[1]}%)`)
+    .map(dscrpt => color(dscrpt))
+  return cols
+}
+
+//TODO: DEPRECATE
+// FUNC: complimentVal()
+function complimentVal(hue = 180) {
+  return (round(hue + 180) % 360)
+}
+//TODO: DEPRECATE
+// FUNC: randomHighHue()
+function randomHighHue() {
+  let hue = floor(R.random_num(0, 255))
+  return color(`hsb(${hue}, 100%, 100%)`)
+}
+//TODO: DEPRECATE
+// FUNC: randomShadHue()
+function randomShadHue() {
+  let hue = floor(R.random_num(0, 255))
+  return color(`hsb(${hue}, 100%, 50%)`)
+}
+
+// slice exp series
+// function sliceExpSeries(min, max) {
+//   let startIndex = expSeries.findIndex(e => e >= min)
+//   let endIndex = expSeries.findIndex(e => e >= max)
+//   let maxInterval = expSeries.slice(endIndex - 1, endIndex + 1)
+//   let scalar = convertRange(max, maxInterval, [0.5, 1])
+//   let scaledLastValue = maxInterval[1] * scalar
+//   let series = expSeries.slice(startIndex, endIndex)
+//   series.push(scaledLastValue)
+//   // print(`min: ${min}, max: ${max}`)
+//   // print(`start: ${startIndex}, end: ${endIndex}`)
+//   print(`scalar: ${scalar}, maxInterval: ${maxInterval}`)
+//   // print(expSeries.slice(startIndex, endIndex))
+//   print(`series: ${series}`)
+//   return series
+// }
+//#endregion
+
+// FUNC: sliceExpSeries()
+function sliceExpSeries(min, max) {
+  let startIndex = expSeries.findIndex(e => e >= min)
+  let endIndex = expSeries.findIndex(e => e > max) + 1
+  let maxInterval = expSeries.slice(endIndex - 2, endIndex)
+  let scalar = convertRange(max, maxInterval, [0, 1])
+  let scaledLastValue = maxInterval[1] * scalar
+  let series = expSeries.slice(startIndex, endIndex)
+    .map(e => e * scalar)
+  // print(`min: ${min}, max: ${max}`)
+  // print(`start: ${startIndex}, end: ${endIndex}`)
+  // print(`scalar: ${scalar}, maxInterval: ${maxInterval}`)
+  // print(expSeries.slice(startIndex, endIndex))
+  // print(`series: ${series}`)
+  return series
+}
+
+// FUNC: createSlices()
+function createSlices(min, max, factor = 0.5) {
+  if (max < min) {
+    return [min]
+  } else {
+    const slice = createSlices(min, max * factor, factor)
+    slice.push(max)
+    // print(slice)
+    return OpArray.from(slice)
+  }
+}
+
+// FUNC: cleanSlices()
+function cleanSlices(min, max, factor = 0.5) {
+  return createSlices(min, max, factor)
+    .map(e => round(e))
+    .unique()
+}
+
+// TODO: Probably DEPRECATE??
+// String Formatting FUNCS
+function cuddle(a, b) {
+  return join([a, b], "")
+}
+
+function px(number) { return `${number}px` }
+function percent(number) { return `${number}%` }
+
