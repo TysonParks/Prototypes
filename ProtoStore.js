3f71b6553e263970737bddc65290d48ebcd86eb9
Tyson Parks <typarks@gmail.com>
2023-03-30 17:28:25 -0700

    Add padding 2 to Effects store

--- ProtoStore.js
+++ ProtoStore.js
@@ -14,7 +14,7 @@ class Store {
     this.Grammars = new ProtoStorage('grmr', 2)
     this.Looks = new ProtoStorage('look', 2)
     this.Actions = new ProtoStorage('axn')
-    this.Effects = new ProtoStorage('fx')
+    this.Effects = new ProtoStorage('fx', 2)
   }
 
   get allLayers() {
