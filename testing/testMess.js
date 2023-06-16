41de64475ad64971ddb33f3d9367f5dc20e5b1d0
Tyson Parks <typarks@gmail.com>
2023-06-15 17:10:15 -0700

    migrate massive unused test code from sketch.js

--- testing/testMess.js
+++ testing/testMess.js
@@ -653,6 +653,405 @@ function styleP6() {
   );
 }
 
+
+// NOTE: Migrated following funcs from sketch.js on June 15, 2023
+// MARK: Recent Test Functions
+// #region Recent Test Functions
+// FUNC: gridTests()
+function gridTests() {
+  let gridX = R.random_int(2, 10)
+  gridX = 4
+  grid = new Grid({ protoParent: F, gridSize: { x: gridX, y: gridX * 2 } })
+  F.inset(0.9)
+  grid.inset(0.9)
+
+  let sizer1 = R.random_num(0.1, 1)
+  let sizer2 = R.random_num(0.1, 1)
+  let funrange = range(0, round(gridX))
+  let funrange2 = range(1, round(gridX * 1.2))
+  // grid.randomComb({ keepRange: funrange, dropRange: funrange2 })
+  // grid.randomComb({ keepRange: range(2, 8), dropRange: range(2, 8) })
+  grid.insetCells(sizer1)
+  grid.insetCells((1 - sizer1), 'grp000')
+  // grid.randomComb({ keepRange: range(1, 2), dropRange: range(1, 10) })
+  // grid.insetCells((1 - sizer2), 'grp001')
+
+
+  // grid.insetCells(sizer1)
+  // grid.insetCells((0.6), 'grp000')
+
+  // grid.comb({ keep: 7, drop: 5 })
+  // grid.outlineGroup(0)
+  grid.randGroup(0.005)
+  grid.outlineTaken(Direction.All)
+  grid.randGroup(0.025)
+  grid.outlineTaken(Direction.Vertical)
+  grid.randGroup(0.05)
+  // grid.outlineTaken(Direction.Horizontal)
+  // grid.randGroup(0.05)
+  grid.randGroup(0.15)
+  grid.outlineTaken(Direction.Ordinal)
+
+  // grid.randGroup(0.05)
+  // grid.outlineTaken()
+  // grid.groupAvail()
+  // grid.insetCells((sizer2), 'grp002')
+  // grid.randomComb()
+  // grid.comb({ keep: 4, drop: 1 })
+  // grid.randomComb()
+  // grid.randomComb()
+  // grid.randomComb()
+
+
+  // print(S.Cells.lastIndex)
+  // print(S.Cells.nextIndex)
+  // print(grid.cells)
+  // print(grid.validNeighbors([grid.cells[0]]))
+
+  // print('all layers:')
+  // print(S.allLayers)
+
+  grid.findIslands({
+    selection: grid.cells,
+    // groupID: 'grp000',
+    direction: Direction.Cardinal,
+  })
+
+  grid.islands.forEach(e => e.createShape())
+
+  // print(grid.islands[0].shape)
+  // print('Frame parentBounds:')
+  // print(F.parentBoundsRect)
+  // print('Frame bounds:')
+  // print(F.boundsRect)
+  // print('Frame insetBounds:')
+  // print(F.insetBoundsRect)
+  // print('Grid bounds:')
+  // print(grid.boundsRect)
+  // print('Grid insetBounds:')
+  // print(grid.insetBoundsRect)
+
+  // print('Isl000 bounds:')
+  // print(grid.islands[0].boundsRect)
+  // print('Shape bounds:')
+  // print(grid.islands[0].shape.boundsRect)
+
+  // print(S.Islands.db[0][1].createShape())
+  // print(S.allLayers)
+  // print(S.Islands.db[0][1].cells)
+  // print(grid.islands)
+  // print(grid.islands.map(e => e.cells))
+  // print('')
+  // print(grid.islands[0].isSingle)
+  // print(grid.islands[0].isCardinalSingle)
+  // print(grid.islands[0].isHorizontal)
+  // print(grid.islands[0].isVertical)
+  // print(grid.islands[0].isOrdinal)
+  // print(S.Cells.db[0][1].sides)
+  // print(S.Islands.db[0][1].exposedSegments)
+  // print('start vertNormals call')
+  // print(grid.vertNormals({ cellIndex: 9, islandID: grid.cellAt(9).islandID }))
+  // print('end vertNormals call')
+
+
+  // print('all layers:')
+  // print(S.allLayers)
+
+
+  // NOTE: Encoding Process Tests
+  print('Encoding Process Tests')
+  const selectIsland = grid.islands[0]
+  const islandCells = selectIsland.cells
+  // print(islandCells)
+  // print(grid.cellBounds(islandCells).size)
+  // print(grid.cellBounds(islandCells).centroidCell)
+  // print(grid.cellBounds(islandCells).centroid)
+  // print(grid.cellBounds(islandCells).takenWeight)
+  // print(grid.cellBounds(islandCells).isMostlyTaken)
+  // print(grid.cellBounds(islandCells).aspect)
+  // print(grid.cellBounds(islandCells).centerOfMass)
+  // print(grid.rowContains(0, 6))
+  // print(grid.columnContains(0, 6))
+  // print(grid.rowContaining(islandCells[0].index))
+  // print(grid.columnContaining(islandCells[0].index))
+  // print(grid.segmentBetween(13, 16))
+  // print(grid.segmentBetween(24, 45))
+  // print(grid.segmentBetween(18, 3))
+  // print('spans')
+  // print(grid.spanFrom(0, 7))
+  // print(grid.spanFrom(6, 47))
+
+  // let islandCellBounds = grid.cellBounds({ selection: islandCells, islandID: selectIsland.id })
+  let islandCellBounds = selectIsland.cellBounds
+  // print(islandCellBounds)
+  // print(islandCellBounds.selection)
+  // print(islandCellBounds.availableCells)
+  // print(islandCellBounds.boundsCells)
+  print('center of mass:')
+  print(islandCellBounds.centerOfMass)
+  print('encoder rotation:')
+  print(islandCellBounds.encoderRotation)
+  print(``)
+  // print(islandCellBounds.encoderRotDegrees)
+  // print('boundsCells:')
+  // print(islandCellBounds.boundsCells)
+  // print('boundCellRows:')
+  // print(islandCellBounds.boundCellRows)
+  // print('encoderCells:')
+  // print(islandCellBounds.encoderCells)
+  // print('horCellIslands:')
+  // print(islandCellBounds.horCellIslands)
+  // print('vertCellIslands:')
+  // print(islandCellBounds.vertCellIslands)
+  // print(`encodingCellCount: ${islandCellBounds.encodingCellCount}`)
+  // print('hor EncodingWeight:')
+  // print(islandCellBounds.encodingWeight(Direction.Horizontal))
+  // print('vert EncodingWeight:')
+  // print(islandCellBounds.encodingWeight(Direction.Vertical))
+
+  print(`Encoding costs: ${islandCellBounds.encodingCosts.string}`)
+  print(``)
+  print(`efficiency: ${islandCellBounds.encodingEfficiency}`)
+  print(``)
+
+  print(grid.cellBounds())
+
+  print('')
+  print('Class List:')
+  print(S.Cells.db[0][1].p5Elt.elt.classList)
+  print('')
+
+  // let grid = new Grid(gridParent, { x: 20, y: 20 })
+  // let drawnGrid = grid.drawCellsAsDivs()
+  // print(BG.elt.position())
+  // print(F.anchor)
+  // print(grid.cells)
+  // print(grid.groups[0].neighborIsInGroup(1, Direction.Right))
+  // print(grid.groups[0].exposedSides(5))
+
+  // print(grid.availableCells)
+  // print(drawnGrid)
+
+  print('all layers:')
+  print(S.allLayers)
+  // print(`this:`)
+  // print(this)
+
+  // makeTestSVG(grid.islands[0])
+}
+
+// FUNC: makeTestSVG()
+function makeTestSVG(parent) {
+  let testSVG = createSVG(400, 400)
+  testSVG
+    .parent(parent.p5Elt)
+    .html('test0000')
+    .position(0, 0)
+    .attribute(SVG.viewBox, `0, 0, 400, 400`)
+    .id('test0000')
+  // .addClass(`testParent`)
+  // .attribute(`x`, `0`)
+  // .attribute(`y`, `0`)
+  // .attribute(SVG.width, `400`)
+  // .attribute(SVG.height, `400`)
+  // .attribute(SVG.style, `padding : 20`)
+  // .attribute(SVG.style, `color : green`)
+  // .attribute(SVG.style, `position : 300 100`)
+  // .attribute(SVG.fill, `green`)
+  testSVG.elt.classList.value = 'someClass otherClass'
+  testSVG.elt.classList.add('thisClass')
+  testSVG.elt.classList.add('thatClass')
+
+  // print(`testSVG:`)
+  // print(testSVG)
+
+
+  let rectSVG = createSVGElt(`rect`)
+  rectSVG
+    .attribute('x', '0')
+    .attribute('y', '0')
+    .attribute('width', '400')
+    .attribute('height', '400')
+    // .attribute('r', '100')
+    .attribute(SVG.fill, '#abc9')
+    .parent(testSVG)
+
+  let circleSVG = createSVGElt(`circle`)
+  circleSVG
+    .attribute('cx', '200')
+    .attribute('cy', '200')
+    .attribute('r', '100')
+    .attribute(SVG.fill, 'red')
+    .parent(testSVG)
+
+
+
+  print(`testSVG:`)
+  print(testSVG)
+  // print(testSVG.elt.namespaceURI)
+}
+
+// FUNC: drawContainer()
+function drawContainer() {
+  container
+    .size(0.8 * frameSize.x, 1.6 * frameSize.x)
+    .style(CS.borderRadius, shapeDef())
+    // .style(CS.background, "#492761")
+    .style(CS.transform, `skew(${testShapeControls.xSkew}deg, ${testShapeControls.ySkew}deg)`)
+    .style(CS.boxShadow, neuBoxShadowFactory(
+      globalControls.baseColor,
+      globalShadowVector().mult(1),
+      globalControls.start,
+      globalControls.spread,
+      globalControls.inset))
+  // .style("will-change", CS.filter)
+  // .style(CS.filter, 'blur(4px)')
+  // .style('CS.transform', "scaleX(-1)")
+}
+
+// FUNC: drawClone()
+function drawClone() {
+  clone
+    .size(0.8 * frameSize.x, 1.6 * frameSize.x)
+    .style(CS.borderRadius, shapeDef())
+    // .style(CS.background, "#e5000f")
+    // .style(CS.transform, `skew(${testShapeControls.xSkew}deg, ${testShapeControls.ySkew}deg)`)
+    .style(CS.scale, `${testShapeControls.cloneScale}`)
+    .style(CS.boxShadow, neuBoxShadowFactory(
+      globalControls.baseColor,
+      globalShadowVector(),
+      globalControls.start,
+      globalControls.spread,
+      (globalControls.same ? globalControls.inset : !globalControls.inset)))
+  // .style('CS.transform', "scaleX(-1)")
+}
+
+// FUNC: drawSquircle()
+function drawSquircle(shape, wrapper) {
+  let path2 = squirclePath(testSquircleControls.width, testSquircleControls.height, testSquircleControls.curveWidth, testSquircleControls.curveHeight, testSquircleControls.flareMode)
+
+  shape
+    .style(CS.backgroundColor, bgCol)
+    // .style(CS.background, 'linear-gradient(135deg, #ccc, #fff)')
+    .size(testSquircleControls.width, testSquircleControls.height)
+    .style(CS.clipPath, path2)
+    .style("shape-outside", path2)
+    .style("overflow", "visible")
+
+  wrapper
+    // .style(CS.backgroundColor, "transparent")
+    .style("will-change", CS.filter)
+    .style("overflow", "visible")
+    .size(testSquircleControls.width, testSquircleControls.height)
+    .style(
+      CS.filter,
+      "drop-shadow(1px 1px 1px #BDBDBD) drop-shadow(-1px -1px 1px #FFFFFF) "
+      + "drop-shadow(2px 2px 2px #BDBDBD) drop-shadow(-2px -2px 2px #FFFFFF) "
+      + "drop-shadow(4px 4px 4px #BDBDBD) drop-shadow(-4px -4px 4px #FFFFFF) "
+      // + "drop-shadow(8px 8px 8px #BDBDBD) drop-shadow(-8px -8px 8px #FFFFFF) "
+      // + "drop-shadow(16px 16px 16px #BDBDBD) drop-shadow(-16px -16px 16px #FFFFFF)"
+      // + "drop-shadow(32px 32px 32px #BDBDBD) drop-shadow(-32px -32px 32px #FFFFFF)"
+      // + "drop-shadow(64px 64px 64px #BDBDBD) drop-shadow(-64px -64px 64px #FFFFFF)"
+    )
+}
+
+// FUNC: drawCurvedShape()
+function drawCurvedShape({ shape, wrapper, drawPoints = false }) {
+  // let path = simpleSquare
+  let path1 = roundedCornerShape({ shape: testShape5 })
+  // let path = squirclePath(400, 200, 0.6, 0)
+  let path2 = roundedCornerShape({ shape: testShape5a })
+  // path2 = ""
+  let path3 = roundedCornerShape({ shape: testShape5b })
+  let path4 = roundedCornerShape({ shape: testShape5c })
+
+  let path = `path('${path1} ${path2} ${path3} ${path4}')`
+  // path = `path('${path1}')`
+  // print(path)
+
+  let points
+
+  if (drawPoints) {
+    points = drawPointsAtVerts({
+      path: path,
+      parent: wrapper,
+      size: 5,
+      color: '#F07',
+      indices: false,
+    })
+  }
+
+  shape
+    .style(CS.backgroundColor, "#AAA")
+    // .style(CS.background, 'linear-gradient(135deg, #ccc, #fff)')
+    .size(600, 600)
+    .style(CS.clipPath, path)
+    .style(CS.shapeOutside, path)
+    .style("overflow", "visible")
+  // .style("outline", 'dashed blue')
+
+  wrapper
+    .style("will-change", CS.filter)
+    .style("overflow", "visible")
+    .size(600, 600)
+}
+
+// FUNC: shapeDef()
+function shapeDef() {
+  return `${cornerDef(testShapeControls.topLeadRadius, testShapeControls.topLeadPercent)} ${cornerDef(testShapeControls.toptTrailRadius, testShapeControls.toptTrailPercent)} ${cornerDef(testShapeControls.botTrailRadius, testShapeControls.botTrailPercent)} ${cornerDef(testShapeControls.botLeadRadius, testShapeControls.botLeadPercent)}`
+}
+
+// FUNC: cornerDef()
+function cornerDef(value, percent = true) {
+  if (percent === true) {
+    return `${value}%`
+  } else {
+    let radius = value / 100 * frameSize.x * 0.8
+    return `${radius}px`
+  }
+}
+
+// FUNC: displayFrameRate()
+function displayFrameRate() {
+  // let limited = limit(frameRate().toFixed(1))
+  fpsDisplay
+    .style(CS.padding, '8px')
+    .html(`${frameRate().toFixed(1)} fps`)
+}
+
+// FUNC: displayTime()
+function displayTime() {
+  let time = [hour() % 12, minute(), second()]
+    .map(e => e.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    }))
+  timeDisplay
+    .style(CS.padding, '8px')
+    .html(`${time[0]}:${time[1]}:${time[2]}`)
+}
+
+// FUNC: limit()
+function limit(input = "00", interval = 1000) {
+  function returnInput() { return input }
+  setInterval(returnInput, interval)
+}
+
+// FUNC: displayHash()
+function displayHash() {
+  let hashDisplay = createDiv(tokenData.hash)
+    .parent(Frame)
+    .center('horizontal')
+
+  let testStyle = GridStyle._2x4.name
+
+  let testEnum = createDiv(testStyle)
+    .parent(Frame)
+    .style(CS.padding, '0 20px')
+}
+// #endregion
+
+
 //=========================================================================
 // TODO: think about the significance of this again...
 const directions = {
