08bea2d8308857799e636525921d614813dff176
Tyson Parks <typarks@gmail.com>
2023-06-27 17:11:27 -0700

    add roundToDec(), cleanup, deprecations

--- ProtoUtility.js
+++ ProtoUtility.js
@@ -381,55 +381,56 @@ class EdgePart {
 
 }
 
-// CLASS: Option
-class Option {
-  #category
-  #options
-  #weighted
-  constructor(name, options) {
-    this.#options = OpArray.from(options)
-    this.#category = name
-    this.#weighted = this.#weighOptions()
-  }
+//TODO: DEPRECATE - early version of EnumFeature in ABFeatureScript.js
+// // CLASS: Option
+// class Option {
+//   #category
+//   #options
+//   #weighted
+//   constructor(name, options) {
+//     this.#options = OpArray.from(options)
+//     this.#category = name
+//     this.#weighted = this.#weighOptions()
+//   }
 
-  // PUBLIC
-  get category() { return this.#category }
-  // get none() { return this.#getFeatureNamed('None') }
-  feature(weight) { return this.#getFeature(this.#getFeatureIndex(weight)) }
-  name(weight) { return this.feature(weight)[1] }
-  value(weight) { return this.feature(weight)[2] || `"${this.name(weight)}" feature has no value` }
-  chance(name) { return this.#getFeatureChance(name) }
+//   // PUBLIC
+//   get category() { return this.#category }
+//   // get none() { return this.#getFeatureNamed('None') }
+//   feature(weight) { return this.#getFeature(this.#getFeatureIndex(weight)) }
+//   name(weight) { return this.feature(weight)[1] }
+//   value(weight) { return this.feature(weight)[2] || `"${this.name(weight)}" feature has no value` }
+//   chance(name) { return this.#getFeatureChance(name) }
 
-  // PRIVATE
-  // get #optionsArray() { return Object.entries(this.#options) }
-  get #totalWeight() {
-    return this.#options
-      .map(e => e[1])
-      .reduce((a, b) => a + b, 0)
-  }
+//   // PRIVATE
+//   // get #optionsArray() { return Object.entries(this.#options) }
+//   get #totalWeight() {
+//     return this.#options
+//       .map(e => e[1])
+//       .reduce((a, b) => a + b, 0)
+//   }
 
-  #getFeatureNamed(name) {
-    return this.#getFeature(this.#options.findIndex(e => e[0] === name)) || `"${name}" feature does not exist`
-  }
-  #getFeature(index) { return [this.#category, this.#options[index][0], this.#options[index][2]] }
-  #getFeatureIndex(weight) { return this.#weighted.findIndex(e => between(weight, e[1])) }
-  #getFeatureChance(name) { return this.#getChance(this.#weighted.findIndex(e => e[0] === name)) }
-  #getChance(index) { return this.#weighted[index][2].toFixed(2) }
-  #weighOptions() {
-    let p = []
-    let currentWeight = 0
-    let weightRange = [0, this.#totalWeight]
-    this.#options.forEach(e => {
-      let range = [currentWeight, currentWeight + e[1]]
-      let normRange = normalizeSubRange(range, weightRange)
-      let normWeight = normRange[1] - normRange[0]
-      currentWeight += e[1]
-      p.push([e[0], normRange, normWeight])
-    })
-    return p
-  }
+//   #getFeatureNamed(name) {
+//     return this.#getFeature(this.#options.findIndex(e => e[0] === name)) || `"${name}" feature does not exist`
+//   }
+//   #getFeature(index) { return [this.#category, this.#options[index][0], this.#options[index][2]] }
+//   #getFeatureIndex(weight) { return this.#weighted.findIndex(e => between(weight, e[1])) }
+//   #getFeatureChance(name) { return this.#getChance(this.#weighted.findIndex(e => e[0] === name)) }
+//   #getChance(index) { return this.#weighted[index][2].toFixed(2) }
+//   #weighOptions() {
+//     let p = []
+//     let currentWeight = 0
+//     let weightRange = [0, this.#totalWeight]
+//     this.#options.forEach(e => {
+//       let range = [currentWeight, currentWeight + e[1]]
+//       let normRange = normalizeSubRange(range, weightRange)
+//       let normWeight = normRange[1] - normRange[0]
+//       currentWeight += e[1]
+//       p.push([e[0], normRange, normWeight])
+//     })
+//     return p
+//   }
 
-}
+// }
 
 // MARK: Utility
 // FUNC: gridPointIndex() calculates 2D array index given coords(x,y) and array width
@@ -491,6 +492,12 @@ function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
 
+// FUNC:
+function roundToDec(number, decimalPlaces) {
+  const factor = 10 ** decimalPlaces;
+  return Math.round(number * factor) / factor;
+}
+
 // TODO: consider the intersection with the DOM Range interface
 // CLASS: Range
 function range(start = 0, end = 1) { return new Range(start, end) }
@@ -523,6 +530,11 @@ class Range {
   cycle(x) { return ((x - this.start) % this.size + this.size) % this.size + this.start }
 }
 
+// Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+const rangeArray = (start, stop, step = 1) => OpArray.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
+
+//TODO: DEPRECATE? - included in ABFeatureScript.js
 // MARK: Helper Methods
 function between(x, range = [0, 1]) { return x >= range[0] && x <= range[1] }
 function convertRange(value, r1, r2) { return (value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0] }
@@ -531,65 +543,8 @@ function normalizeSubRange(subrange, range) {
   return [normalize(subrange[0], range), normalize(subrange[1], range)]
 }
 
-// Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-const rangeArray = (start, stop, step = 1) => OpArray.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
-
-// TODO: implement Shape and Puzzle
 
 
 
-// CLASS: Puzzle
-class Puzzle {
-  shapes
-
-  constructor(shapes) {
-    this.shapes = shapes
-  }
-}
 
 
-// MARK: DEPRECATE
-// #region DEPECATE
-// TODO: This might be completely unnecessary because I can and probably should just assign callbacks externally using css selectors after all the layout creation magic has completed and we have a remaining set of ProtoLayers - CSS FTW!
-// CLASS: StateObserver
-// NOTE: https://stackoverflow.com/a/60701062, https://jsfiddle.net/leviwheatcroft/wqf9jL2g/42/
-// Instead of 3 classes (1 super and two subs) I put both Observed and Reactive methods into the same class because I know each ProtoLayer will need to be able to do both
-class StateObserver {
-  instances
-  state
-  lastState
-  action
-
-  constructor() {
-    StateObserver.instances.push(this)
-  }
-
-  stateUpdate(update) {
-    StateObserver.lastState = StateObserver.state
-    StateObserver.state = update
-    StateObserver.instances.forEach(i => {
-      if (!i.onStateUpdate) {
-        return i.onStateUpdate(update, StateObserver.lastState)
-      }
-    })
-  }
-
-  connectedCallback(eltID = this.id, trigger) {
-    select(elementID).mousePressed(trigger)
-  }
-
-  onStateUpdate(state, lastState, reaction = this.reaction) {
-    if (state.reaction === lastState.reaction) {
-      reaction
-    }
-  }
-
-
-}
-
-StateObserver.instances = []
-StateObserver.state = {}
-StateObserver.lastState = {}
-
-//#endregion
