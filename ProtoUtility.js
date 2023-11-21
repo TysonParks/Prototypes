e6497bbf71e956c6548a1b01ce3e0736b6b2e72c
Tyson Parks <typarks@gmail.com>
2023-11-21 07:32:59 -0800

    repair Direction.random

--- ProtoUtility.js
+++ ProtoUtility.js
@@ -157,7 +157,7 @@ class Direction {
   random(amount = 1) {
     const reducer = min(amount / this.vals.length, 0.999999)
     //NOTE: when 'copy' is removed here it creates a cool shadow stacking effect with findIslands (see Aug 2,2023 captures)
-    // return new Direction(this.vals.copy.randReduce(reducer))
+    return new Direction(this.vals.copy.randReduce(reducer))
     return new Direction(this.vals.randReduce(reducer))
   }
 
