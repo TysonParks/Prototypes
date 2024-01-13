b6700046e8bbdcec6e88999ec19a96b1885fbe72
Tyson Parks <typarks@gmail.com>
2024-01-13 07:38:29 -0800

    change testing

--- sketch.js
+++ sketch.js
@@ -365,7 +365,7 @@ function gridTests2() {
   // grid.outlineGroup({ groupID: grid.lastGroup.id, direction: Direction.All, newGroup: false, amount: R.random_int(1, 1) })
   // const randDir = Direction.All.random(3)
   // console.log('randDirection', randDir)
-  let group001 = grid.outlineGroup({ groupID: grid.lastGroup.id, direction: Direction.All, newGroup: true, amount: R.random_int(1, 2) })
+  let group001 = grid.outlineGroup({ groupID: grid.lastGroup.id, direction: Direction.All, newGroup: true, amount: R.random_int(1, 1) })
   // let group002 = grid.outlineGroup({ groupID: grid.lastGroup.id, direction: Direction.All, newGroup: true, amount: R.random_int(1, 1) })
 
   let group002 = grid.randGroup({ amount: 0.5 })
