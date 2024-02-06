d5e6da78e50205740eb21c39677d7535de01ade6
Tyson Parks <typarks@gmail.com>
2024-02-06 12:07:40 -0800

    add cutAllToCardinal()

--- Unused.js
+++ Unused.js
@@ -1,10 +1,49 @@
 //MARK: Useful code that went unused!
 
-
-
-
 class UnusedSegPath {
+  //METH: cutAllToCardinal()
+  static cutAllToCardinal(segPath) {
 
+    //ARROW: shared() : find segments with shared startPoint to input seg's endPoint
+    const shared = (seg) => {
+      const pairs = segPath.filter(s => seg.end.equals(s.start, 4)) // seg.end = s.start
+      if (pairs.length === 2) { return [seg, pairs] } // ordinal connections will have two connections 
+    }
+
+    const sharedStarts = segPath.map(seg => shared(seg)).compacted // find all ordinal corner segments
+    console.log(`sharedStarts`, sharedStarts)
+
+    sharedStarts.forEach(s => { // swap segment neighbors and remove cubic verts at corners
+      const [seg, [nextA, nextB]] = s
+      let newNeighbor
+      if (seg.neighbors.end.id === nextA.id) { newNeighbor = nextB }      // nextA was initial neighbor
+      else if (seg.neighbors.end.id === nextB.id) { newNeighbor = nextA } // nextB was initial neighbor
+      else { console.error(`cutAllToCardinal Error: unexpected case hit, please investigate!`) } // Error just in case
+      // console.log(`${seg.hasBothCubicVerts}`)
+      seg.assignNeighbors({ end: newNeighbor })   // swap seg's endNeighbor
+      seg.clearCubicEndVerts()                      // clear seg's CubicEndVerts 
+      newNeighbor.assignNeighbors({ start: seg }) // swap newNeighbor's startNeighbor
+      newNeighbor.clearCubicStartVerts()            // clear newNeighbor's clearCubicStartVerts 
+      // console.log(`${seg.hasBothCubicVerts}`)
+    })
+
+    let newPaths = new OpArray
+    let oldPath = segPath.copy
+    while (oldPath.length > 0) {
+      const first = oldPath[0]
+      const newPath = first.sortedSegPath
+      // console.log(`newPath`, newPath)
+      newPaths.push(newPath)
+      oldPath = oldPath.exclude(newPath, ['id'])
+    }
+    console.log(`newPaths: ${newPaths.map(path => path.map(seg => seg.hasBothCubicVerts))}`)
+
+
+    //FIXME: NEXT STEP: recalculate corners!
+    //FIXME: FINAL STEP: return paths and assign them as new subshapes in new shape copy
+    //FIXME: ALSO: add an early bailout if no sharedStarts are found, just return original segPath
+    return newPaths
+  }
 }
 
 
