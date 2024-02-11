9a57fc75425035822f1707e9a9f868b7215ee006
Tyson Parks <typarks@gmail.com>
2024-02-11 08:41:09 -0800

    add randomExtended, documentation

--- ProtoStore.js
+++ ProtoStore.js
@@ -46,17 +46,21 @@ class ProtoStorage {
   shortName
   #pad
   #db = new OpArray
+
   constructor(shortName, pad = 3) {
     this.shortName = shortName
     this.#pad = pad
   }
 
+  //MARK: Computed
   get length() { return this.#db.length }
   get lastIndex() { return this.#db.lastIndex }
   get nextIndex() { return (this.lastIndex + 1).toString().padStart(this.#pad, '0') }
   get nextKey() { return `${this.shortName}${this.nextIndex}` }
   get db() { return this.#db }
 
+  //MARK: Methods
+  //METH: add() :
   add(value) {
     // let alreadyAssigned = this.#db.findIndex(e => ProtoLayer.equal(e, value))
     // if (alreadyAssigned) {
@@ -67,18 +71,19 @@ class ProtoStorage {
     return nextKey
     // }
   }
+  //METH: remove() :
   remove(key) {
     let index = this.#db.findIndex(e => e[0] === key)
     if (index) { this.#db.splice(index, 1) }
   }
-
+  //METH: named() :
   named(id) { return this.item({ key: id }) }
-
+  //METH: random() :
   random(count = 1) {
     let items = rangeArray(1, count)
     return items.map(e => this.#db[R.random_int(0, this.lastIndex)])
   }
-
+  //METH: item() :
   item({ index, key } = {}) {
     if (arguments.length === 0) { return this.random(1) }
     if (index && key) { return }
@@ -91,23 +96,35 @@ class ProtoStorage {
 const storedProtocol = {
   store: 'unassigned',
   id: 'unassigned',
-
+  //METH: storeObject() :
   storeObject(store) {
     if (store) {
       this.store = store
       this.assignID()
     }
   },
-
+  //METH: assignID() :
   assignID() { this.id = this.store.add(this) }
 }
 
 // MIXIN: identifiableProtocol: a mixin for assigning uids to objects
 const identifiableProtocol = {
   uid: 'unassigned',
+  //METH: assignUID() :
   assignUID() { this.uid = RuID.random_hash() }
 }
 
 // MIXIN: identifiableStored: a mixin composition of storedProtocol and identifiableProtocol
 const identifiableStored = Object.assign({}, storedProtocol, identifiableProtocol)
 
+// MIXIN: randomExtended: mixin to add UID hash creation functionality to Random
+const randomExtended = {
+  //METH: random_hash : default length 4 -> 16bit hash
+  random_hash(length = 4, prefix = '0x') {
+    let x = "0123456789abcdef", hash = prefix
+    for (let i = length; i > 0; --i) {
+      hash += x[Math.floor(this.random_dec() * x.length)]
+    }
+    return hash
+  }
+}
