e029206b5a7013dad546fe40a7c868cf3c4b3f12
Tyson Parks <typarks@gmail.com>
2024-02-11 08:39:44 -0800

    add debuggableProtoLayer

---
+++ DeBugging.js
@@ -0,0 +1,45 @@
+// MIXIN: debuggableProtoLayer : mixin for displaying debug info
+const debuggableProtoLayer = {
+  drawLabel: false,
+  drawRect: false,
+  drawPerimeter: false,
+  drawInset: false,
+  drawLoft: false,
+
+  //MARK: Show Methods
+  showDeBug() {
+    if (drawLabel) { showLabel() }
+    if (drawRect) { showRect() }
+    if (drawPerimeter) { showPerimeter() }
+    if (drawInset) { showInset() }
+    if (drawLoft) { showLoft() }
+  },
+
+  showLabel() {
+    const label = createSVGText(this.id, 0, 0)
+    const isShape = this.type !== `Shape`
+    const offset = isShape ? vert(1, 4) : vert(1, 8)
+    const font = isShape ? `bold 3px sans-serif` : `3px sans-serif`
+
+
+
+    label
+      .parent(this.svgElt)
+      .addToClassList(this.id)
+      .addToClassList(this.svgParent.elt.classList.value)
+      .layout(this.anchor.x + offset.x, this.anchor.y + offset.y, this.size.x, this.size.y)
+      .style(`font`, font)
+  },
+  showRect() { },
+  showPerimiter() { },
+  showInset() { },
+  showLoft() { },
+
+  //MARK: Computed Methods
+  calcAnchor() {
+    if (this.cellBounds) {
+
+    }
+  }
+
+}
