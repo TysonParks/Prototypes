4e7222e91ae31119c4a5631f9d8bc8f4cd36561b
Tyson Parks <typarks@gmail.com>
2024-11-01 10:38:56 -0700

    Remove static empty.

--- OpArray.js
+++ OpArray.js
@@ -11,8 +11,8 @@ class OpArray extends Array {
     super(...args)
   }
 
-  static get empty() { return OpArray.from([]) }
   // take a single value, or an array/OpArray and make/ensure its an OpArray
+
   static format(values) {
     if (!(values instanceof OpArray)) {
       if (!(values instanceof Array)) {
