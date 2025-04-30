ddcae15256a463fcc1fcdf4e2d2fbb6bf49a98f3
Tyson Parks <typarks@gmail.com>
2025-04-30 12:57:40 -0700

    Add UnusedShade,   SVGLook, & Look classes.

--- Unused.js
+++ Unused.js
@@ -1132,9 +1132,385 @@ class UnusedSelectionBounds extends SelectionBounds {
   // #endregion
 }
 
+//CLASS: UnusedShade
+class UnusedShade {
+  // MARK: OG CSS Methods
+  // #region OG CSS Methods
+  //METH: Box-Shadow CSS
+  static boxShadCSS(x, y, blurRad = 0, spreadRad = 0, col = color(0), inset = false) {
+    let color = col.toString('#rrggbb')
+    if (inset === true) {
+      return `inset ${x}px ${y}px ${blurRad}px ${color}`
+    } else if (spreadRad === 0) {
+      return `${x}px ${y}px ${blurRad}px ${color}`
+    } else {
+      return `${x}px ${y}px ${blurRad}px ${spreadRad}px ${color}`
+    }
+  }
+  //METH: Text-Shadow CSS
+  static textShadCSS(x, y, blurRad = 0, col = color(0)) { return this.boxShadCSS(x, y, blurRad, col) }
+  //METH: Drop-Shadow CSS
+  static dropShadCSS(x, y, blurRad = 0, col = color(0)) {
+    return `drop-shadow(${this.boxShadCSS(x, y, blurRad, col)})`
+  }
+  //METH: Neumorphic Box-Shadow CSS - create highlight shadow pair code for CSS
+  static neuBoxShadCSS(vector = this.shadVect(), blurRad, highCol, shadCol, inset = false) {
+    let highlightCSS = this.boxShadCSS(-vector.x, -vector.y, blurRad, 0, highCol, inset)
+    let shadowCSS = this.boxShadCSS(vector.x, vector.y, blurRad, 0, shadCol, inset)
+    return `${shadowCSS}, ${highlightCSS}`
+  }
+  //METH: Neumorphic Box Shadow Factory - create a shadow and highlight stack
+  static neuBoxShadFactory({ baseCol = protoColor(230), vector = this.shadVect(), start = 0.5, spread = 16, inset = false } = {}) {
+    // DeBug.log('neuCSS')
+    // DeBug.log(baseCol, vector, start, spread, inset)
+    let offset = vector.mag() / sqrt(2)
+    let cols = baseCol.highShadSpread(spread)
+    // DeBug.log(offset, cols)
+    let neuShads = cleanSlices(start, offset, globalControls.shadQuality)
+    print(neuShads.map(e => e.toFixed(2)))
+    neuShads = neuShads.map(sliceOffset => this.neuBoxShadCSS(vector.setMag(sliceOffset), 2 * sliceOffset, cols[0], cols[1], inset))
+    return neuShads
+  }
+  // #endregion
+}
+
+// CLASS: SVGLook
+// SIZE: 70 lines
+class SVGLook {
+  static prototypeFunctions() {
+    // PROTOTYPE: svgLook(look) : p5.Element : add SVG look to element
+    // p5.Element.prototype.svgLook = function (look) {                                            //UNUSED:
+    //   if (look instanceof Array) {
+    //     let lookOp = OpArray.from(look).compacted
+    //     if (look[0][0] instanceof Array) lookOp = lookOp.reduce((acc, val) => acc.concat(val), [])
+    //     lookOp.forEach(e => this.attribute(e[0], e[1]))
+    //   }
+    //   return this
+    // }
+
+    // PROTOTYPE: label(text, color, direction) : p5.Element : add label to element
+    // p5.Element.prototype.label = function (text, color, direction) {                            //UNUSED:
+    //   const labelText = createSVGElt("text")
+    //   labelText.html(text)
+    //   labelText.style("fill", color)
+
+    //   const
+    //     parentBBox = this.elt.getBBox(),
+    //     xFactor = direction.isNone ? 0.5 : Math.cos(direction.angle),
+    //     yFactor = direction.isNone ? 0.5 : Math.sin(direction.angle),
+    //     x = parentBBox.x + (parentBBox.width * (xFactor + 1)) / 2,
+    //     y = parentBBox.y + (parentBBox.height * (yFactor + 1)) / 2
+
+    //   labelText.attribute("x", x)
+    //   labelText.attribute("y", y)
+    //   labelText.attribute("text-anchor", "middle")
+    //   labelText.attribute("dominant-baseline", "central")
+
+    //   this.child(labelText)
+    //   return this
+    // }
+  }
+
+  static get blackAndWhite() { return SVGLook.clear }
+  static get clear() {
+    return [
+      ['fill-opacity', `0`],
+      ['stroke-opacity', `0`],
+    ]
+  }
+  //MARK: Methods
+  //METH: testStroke() : [OpArray] : create stroke look for SVG 
+  static testStroke(color = ProtoColor.randomHighHue(), opacity = 1, radius = 5) {
+    return [
+      ['stroke', color],
+      ['stroke-opacity', `${opacity}`],
+      ['stroke-width', '.5'],
+      // ['pathLength', '360'],
+      ['stroke-dasharray', `0 2`],
+      ['stroke-linecap', 'round'],
+      ['stroke-linejoin', 'round'],
+      ['rx', `${radius}`],
+      ['ry', `${radius}`],
+      ['fill-opacity', '0'],
+    ]
+  }
+  //METH: testFill() : [OpArray] : create fill look for SVG
+  static testFill(color = ProtoColor.randomHighHue(), opacity = .5, radius = 5) {
+    return [
+      ['fill', color],
+      ['fill-opacity', `${opacity}`],
+      ['rx', `${radius}`],
+      ['ry', `${radius}`],
+    ]
+  }
+  //METH: test() : [OpArray] : create test look for SVG
+  static test(strokeColor, fillColor, opacity = .5, radius = 5) {
+    return [...SVGLook.testStroke(strokeColor, 1, radius), ...SVGLook.testFill(fillColor, `${opacity}`)]
+  }
+  //METH: blackAndWhite
+  static blackAndWhite() { }
+
+  static get clear() {
+    return [
+      ['fill-opacity', `0`],
+      ['stroke-opacity', `0`],
+    ]
+  }
+  //METH: trendyCactus() : [OpArray] : create trendy cactus look for SVG
+  static trendyCactus(path) {
+    const
+      length = path.elt.getTotalLength(),
+      dashLength = R.random_int(0, 20),
+      dashWidth = (20 - dashLength) / 4,
+      loopCount = R.random_int(0, 10),
+      //  loopCount = 10,
+      offset = R.random_num(0, 10)
+    return [
+      ['fill', ProtoColor.randomHighHue()],
+      ['fill-opacity', '0.2'],
+      ['stroke', 'black'],
+      ['stroke-opacity', '1'],
+      ['stroke-linecap', 'round'],
+      ['stroke-linejoin', 'round'],
+      ['overflow', 'auto'],
+      ['pathLength', 'length'],
+      ['stroke-dasharray', `${dashLength} ${length / loopCount - dashLength}`],
+      ['stroke-width', `${dashWidth}`],
+      ['stroke-dashoffset', `${offset}`],
+    ]
+  }
+}
+
+// CLASS: Look
+// SIZE: 122 lines
+class Look {
+  static prototypeFunctions() {
+    // PROTOTYPE: look(look, html) : p5.Element : add CSS look to element
+    // p5.Element.prototype.look = function (look = Look.testGrid, html) {                         //UNUSED:
+    //   if (look instanceof Array) {
+    //   if (look instanceof Array) {
+    //     const lookOp = OpArray.from(look).compacted
+    //     if (look[0][0] instanceof Array) lookOp = lookOp.reduce((acc, val) => acc.concat(val), [])
+    //     lookOp.forEach(e => this.style(e[0], e[1]))
+    //   }
+    //   if (html) this.html(html)
+    //   return this
+    // }
+
+
+
+    // PROTOTYPE: p5.Element.prototype.boxShadow
+    // p5.Element.prototype.boxShadow = function (value) {                                          //UNUSED:
+    //   return this.style(boxShadow, value)
+    // }
+  }
+
+  static islandShape(path, color = randomColor().toString('#rrggbbaa')) {
+    return [
+      [CS.backgroundColor, testingControls.blackMode ? '#000' : color],
+      [CS.clipPath, path],
+      [CS.shapeOutside, path],
+      // [CS.overflow, 'visible']
+    ]
+  }
+
+  static neuShade(
+    {
+      baseCol = protoColor(230),
+      vector = globalShadowVector(),
+      start = globalControls.start,
+      spread = globalControls.spread,
+      inset = globalControls.inset,
+    } = {}
+  ) {
+    return [[CS.boxShadow, Shade.neuBoxShadFactory(baseCol, vector, start, spread, inset)]]
+  }
+
+  static textAlign({ size, hor = 'center', vert = 'center' } = {}) {
+    let
+      textAlign = [CS.textAlign, hor],
+      lineHeight = [CS.lineHeight, `${size.y}px`],
+      otherHeight = [CS.lineHeight, `${(size.y + (sqrt(size.x) * 2.5))}px`]
+    if (vert === 'center') return [textAlign, lineHeight]
+    if (vert === 'top') return [textAlign]
+    if (vert === 'below') return [textAlign, otherHeight]
+  }
+
+  static testText(
+    sizeX,
+    color,
+    radius = 25,
+    labels = testingControls.labels,
+    borders = testingControls.borders,
+  ) {
+    return [
+      [CS.color, (labels ? color : CS.clear)],
+      // [CS.borderRadius, `${sqrt(sizeX) * 1.5}px`],
+      [CS.borderRadius, `${radius}px`],
+      [CS.fontSize, `${sqrt(sizeX) * 1.5}px`],
+      [CS.border, `${borders ? color : CS.clear} dashed ${sqrt(sizeX) / 20}px`],
+    ]
+  }
+
+  static test(size, type) {
+    switch (type) {
+      case 'frame':
+        return [...Look.textAlign({ size: size, vert: 'top' }), ...Look.testText(size.x, '#80F8', 10)]
+      case 'grid':
+        return [...Look.textAlign({ size: size, vert: 'top', hor: 'start' }), ...Look.testText(size.x, '#08F8')]
+      case 'group':
+        // return []
+        return [...Look.textAlign({ size: size, hor: 'start', vert: 'top', }), ...Look.testText(size.x, '#80F0')]
+      case 'cell':
+        const
+          text = [
+            ...Look.textAlign({ size: size }),
+            ...Look.testText(size.x, '#8088')
+          ],
+          neuShade = [...Look.neuShade()]
+        if (testingControls.blackMode) return text
+        else return [...text, ...neuShade]
+      case 'island':
+        return [...Look.textAlign({ size: size }), ...Look.testText(size.x, '#F08b')]
+      case 'shape':
+        return [...Look.textAlign({ size: size, vert: 'below' }), ...Look.testText(size.x, '#F0Fb')]
+    }
+  }
+
+  static get testGrid() {
+    return [
+      // [CS.color, '#80F'],
+      [CS.color, CS.clear],
+      [CS.backgroundColor, '#00'],
+      [CS.borderRadius, '10%'],
+      [CS.textAlign, "center"],
+      // [CS.border, '#F7F dashed 1px'],
+      // [CS.display, 'none'],
+    ]
+  }
+
+  static testCell(color = randomColor().toString('#rrggbbaa'), labels = testingControls.labels,) {
+    // let randomColor = randomColor().toString('#rrggbbaa')
+    return [
+      [CS.color, (labels ? '#00F' : CS.clear)],
+      [CS.backgroundColor, '#0FF6'],
+      [CS.backgroundColor, color],
+      [CS.borderRadius, '20%'],
+      [CS.textAlign, "center"],
+      // [CS.border, '#808 solid 1px'],
+    ]
+  }
+
+  static blankTestCell(labels = testingControls.labels,) {
+    const neuShade = [...Look.neuShade({ vector: globalShadowVector().mult(0.3), inset: !globalControls.inset, })]
+    return [
+      [CS.color, (labels ? '#00F' : CS.clear)],
+      [CS.backgroundColor, CS.clear],
+      [CS.borderRadius, '50%'],
+      [CS.textAlign, "center"],
+      testingControls.blackMode ? neuShade : null,
+      // neuShade,
+      // ...Look.neuShade({ vector: globalShadowVector().mult(0.3), inset: !globalControls.inset, })
+    ]
+  }
+
+  static centeredFlex(color, direction = 'row') {
+    return [
+      [CS.background, color],
+      [CS.margin, '0'],
+      [CS.display, 'flex'],
+      [CS.flexDirection, direction],
+      [CS.justifyContent, 'space-evenly'],
+      [CS.alignItems, 'center'],
+    ]
+  }
+
+}
+
+// CLASS: CS
+// SIZE: 33 lines
+class CS {
+  static inset = 'inset'
+  // static space = ' '
+  static comma = ', '
+
+  static boxShadow = 'box-shadow'
+  // static dropShadow = 'drop-shadow'
+  static textShadow = 'text-shadow'
+
+  static transform = 'transform'
+  static border = 'border'
+  static borderRadius = 'border-radius'
+  static rotate = 'rotate'
+  static scale = 'scale'
+  static filter = 'filter'
+  static padding = 'padding'
+  static clipPath = 'clip-path'
+  static shapeOutside = 'shape-outside'
+  static fontSize = 'font-size'
+  static textAlign = 'text-align'
+  static vertAlign = 'vertical-align'
+  static lineHeight = 'line-height'
+  static margin = 'margin'
+  // static width = 'width'
+  static background = 'background'
+  static backgroundColor = 'background-color'
+  static color = 'color'
+  static clear = "#0000"
+  static display = 'display'
+  static overflow = 'overflow'
+  static flexDirection = 'flex-direction'
+  static justifyContent = 'justify-content'
+  static alignItems = 'align-items'
+}
+
 class OldestCode {
   //MARK: oldFunctionsNeedToLiveInAMethod!!!
+  slice() {
+    // FUNC: sliceExpSeries()
+    function sliceExpSeries(min, max) {
+      let startIndex = expSeries.findIndex(e => e >= min)
+      let endIndex = expSeries.findIndex(e => e > max) + 1
+      let maxInterval = expSeries.slice(endIndex - 2, endIndex)
+      let scalar = convertRange(max, maxInterval, [0, 1])
+      let scaledLastValue = maxInterval[1] * scalar
+      let series = expSeries.slice(startIndex, endIndex)
+        .map(e => e * scalar)
+      return series
+    }
 
+    // FUNC: createSlices()
+    //NOTE: created with GPT-4 April 18,2023
+    function createSlices(min, max, factor = 0.5) {
+      const slice = []
+
+      function helper(min, max, factor) {
+        if (max >= min) {
+          helper(min, max * factor, factor)
+          slice.push(max)
+        }
+      }
+
+      helper(min, max, factor)
+      return OpArray.from(slice)
+    }
+    // FUNC: exponentialSlices()
+    function exponentialSlices(min, max, amount, factor = 0.5) {
+      // DeBug.log('expSlicesInput', min, max, amount)
+      // if (amount < 3) { return OpArray.from([min, max]) }
+      const range = max - min
+      const multipliers = createSlices(1, pow(2, amount - 1), factor).map(e => e - 1)
+      const last = multipliers.last
+      // DeBug.log('multipliers', multipliers)
+      return multipliers.map(e => min + e * (range / last))
+    }
+    // FUNC: cleanSlices()
+    function cleanSlices(min, max, factor = 0.5) {
+      return createSlices(min, max, factor)
+        .map(e => round(e))
+        .unique()
+    }
+  }
   layout() {
     //NOTE: this was layout.js, created July 5, 2022
 
